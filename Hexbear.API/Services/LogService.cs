348c86e2ed50e41344420ac30cf826a406bd5a30
Anon <makotech222@gmail.com>
2023-08-20 16:01:52 -0500

    f

--- Hexbear.API/Services/LogService.cs
+++ Hexbear.API/Services/LogService.cs
@@ -28,7 +28,8 @@ namespace Hexbear.API.Services
             {
                 if (!File.Exists($@"{container}/hostname"))
                     continue;
-                string hostname = File.ReadAllText($@"{container}/hostname");
+                string hostname = File.ReadAllText($@"{container}/hostname").Trim();
+                Console.WriteLine(hostname);
                 if (!whitelistedHostNames.Contains(hostname))
                     continue;
                 var files = Directory.EnumerateFiles($@"{container}/", "*json.log*", SearchOption.AllDirectories).ToList();
